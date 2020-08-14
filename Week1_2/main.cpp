a3bd04650f89a04cd60dc981c7eb1d264870cdc2
Natashi <32347635+Natashi@users.noreply.github.com>
2020-08-14 17:30:05 +0700

    More snarky comments, and upside-down!!!

--- Week1_2/main.cpp
+++ Week1_2/main.cpp
@@ -4,29 +4,34 @@
 
 using namespace std;
 
+int _abs(int x) {
+    return (x >> 31) ? ((int)(~x) + 1) : x;
+}
+
 int main() {
     int depth;
 
     scanf("%d", &depth);
-    if (depth >= 0) {
-        printf("Ah, yes, pyramids. The staple of modern diet.\nNo, please don't actually eat one.\n");
-        printf("Well, here's your anyways. %d blocks tall, just as you requested.\n", depth);
-
-        if (depth == 0) {
-            printf("\nThat's a very flat pyramid.\n");
-        }
-        else {
-            for (int i = 0; i < depth; ++i) {
-                int pitch = depth - i - 1;
-                int block = (i + 1) * 2 - 1;
-                for (int j = 0; j < pitch; ++j) putchar(' ');
-                for (int j = 0; j < block; ++j) putchar('*');
-                putchar('\n');
-            }
-        }
+    if (depth == 0) {
+        printf("That's a very flat pyramid.\n");
     }
     else {
-        printf("We don't produce upside-down pyramids here.\n");
+        printf("Ah, yes, pyramids. The staple of modern diet.\nNo, please don't actually eat one.\n");
+        printf("Well, here's your anyways. %d block%s tall%s, just as you requested.\n", _abs(depth),
+            _abs(depth) == 1 ? "" : "s", depth < 0 ? ", and upside-down" : "");
+
+        auto PrintRow = [](int width, int iRow) {
+            int pitch = width - iRow - 1;
+            int block = (iRow + 1) * 2 - 1;
+            for (int j = 0; j < pitch; ++j) putchar(' ');
+            for (int j = 0; j < block; ++j) putchar('*');
+            putchar('\n');
+        };
+
+        if (depth > 0)
+            for (int i = 0; i < depth; ++i) PrintRow(depth, i);
+        else
+            for (int i = -depth - 1; i >= 0; --i) PrintRow(-depth, i);
     }
 
     return 0;
